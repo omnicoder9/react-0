9e42a08b50a0047dcc541ed5a67718bb4e672cac
omnicoder9 <nknapp53928@gmail.com>
2023-12-15 11:27:25 -0500

    added props example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Comp1 from './C1';
 import { TodoList } from './C1';
+import { Avatar } from './C1';
+import { getImageUrl } from './utils.js';
 
+
+function Profile({ name, src, width, height, numAwards, awards, profession, discovered }){
+  return (<section className="profile">
+        <h2>{name}</h2>
+        <img
+          className="avatar"
+          src={src}
+          alt={name}
+          width={width}
+          height={height}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b> 
+            {profession}
+          </li>
+          <li>
+            <b>Awards: {numAwards} </b> 
+            {awards}
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {discovered}
+          </li>
+        </ul>
+      </section>);
+  
+}
 function App() {
   return (
     <div className="App">
@@ -13,6 +43,28 @@ function App() {
         </p>
         <Comp1/>
         <TodoList/>
+        <Avatar/>
+        <h1>Notable Scientists</h1>
+      <Profile
+        name={"Katsuko Saruhashi"}
+        src={getImageUrl('YfeOqp2')}
+        width={70}
+        height={70}
+        profession={"geochemist"}
+        numAwards={"2"}
+        awards={"(Miyake Prize for geochemistry, Tanaka Prize)"}
+        discovered={"a method for measuring carbon dioxide in seawater"}
+      />
+      <Profile
+        name={"Maria Skłodowska-Curie"}
+        src={getImageUrl('szV5sdG')}
+        width={70}
+        height={70}
+        profession={"physicist and chemist"}
+        numAwards={"4"}
+        awards={"(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"}
+        discovered={"polonium (chemical element)"}
+      />
         
         <a
           className="App-link"
--- my-app/src/C1.js
+++ my-app/src/C1.js
@@ -7,18 +7,27 @@ export default function Comp1() {
         </div>
     )
 }
+const today = new Date();
 
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
 //use of converter to use existing HTML
 export function TodoList() {
     return (
       <>
-        <h1>Hedy Lamarr's Todos</h1>
+        <h1>Hedy Lamarr's Todos for {formatDate(today)}</h1>
         <img
           src="https://i.imgur.com/yXOvdOSs.jpg"
           alt="Hedy Lamarr"
           className="photo"
         />
-        <ul>
+        <ul style={{
+      backgroundColor: 'black',
+      color: 'pink' }}>
           <li>Invent new traffic lights</li>
           <li>Rehearse a movie scene</li>
           <li>Improve spectrum technology</li>
@@ -26,3 +35,16 @@ export function TodoList() {
       </>
     );
     }
+
+
+    export function Avatar() {
+        const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+        const description = 'Gregorio Y. Zara';
+        return (
+          <img
+            className="avatar"
+            src={avatar}
+            alt={description}
+          />
+        );
+      }
